f2d68bbee9bb2260ed105e3221c62eaca1ef2820
SXT9 <ari.kudrjavcev@gmail.com>
2022-09-27 21:55:03 +0400

    first

---
+++ src/main/java.sprint.test/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String catName;
+
+    public void setCatName(String catName) {
+        this.catName = catName;
+    }
+
+    public String getCatName() {
+        return catName;
+    }
+}
