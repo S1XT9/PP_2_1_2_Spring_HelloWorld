50faaa2f544c593807b18c8e3a7a014e6fdb3e7a
SXT9 <ari.kudrjavcev@gmail.com>
2022-09-27 21:52:47 +0400

    first

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,16 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean1==bean);
+        System.out.println(cat1==cat2);
+        cat1.setCatName("Sonya");
+        cat2.setCatName("Leto");
+        bean.setMessage("Hello");
+        bean1.setMessage("Privet");
+        System.out.println(cat1.getCatName()==cat2.getCatName());
+        System.out.println(bean1.getMessage()==bean.getMessage());
     }
 }
